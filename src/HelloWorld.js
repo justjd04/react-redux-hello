f7f8d4b59d9b499d037031a824b6395ddf99dd36
Jan Dohinog <janreydohinog@gmail.com>
2021-07-17 04:41:33 +0800

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWorld = ({tech}) => {
+    return (
+        <div>
+         Hello World <span>{tech}</span>   
+        </div>
+    )
+}
+
+export default HelloWorld
